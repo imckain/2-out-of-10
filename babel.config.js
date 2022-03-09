b606770a190f8cbfc5b9895393fbea2779b1e4d0
imckain <ianmmckain@gmail.com>
2022-03-08 21:55:12 -0500

    getting google map API integrated for homescreen.

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,11 @@
 module.exports = function(api) {
   api.cache(true);
   return {
+    "plugins": [
+      ["module:react-native-dotenv", {
+        "moduleName": "react-native-dotenv"
+      }]
+    ],
     presets: ['babel-preset-expo'],
   };
 };
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,14 +1,51 @@
 import React from 'react';
-import { View, Text } from 'react-native';
-
+import { View, Text, StyleSheet } from 'react-native';
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+// @ts-ignore
+import { API_KEY } from 'react-native-dotenv';
 
 const HomeScreen = () => {
- 
-  return (
+
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: API_KEY
+  })
+
+  const [map, setMap] = React.useState(null)
+
+  const onLoad = React.useCallback(function callback(map) {
+    const bounds = new window.google.maps.LatLngBounds();
+    map.fitBounds(bounds);
+    setMap(map)
+  }, [])
+
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null)
+  }, [])
+
+  return isLoaded ? (
     <View>
       <Text>balls</Text>
+      <GoogleMap
+        mapContainerStyle={styles.mapStyle}
+        center={{
+          lat: -5,
+          lng: -60,
+        }}
+        zoom={10}
+        onLoad={onLoad}
+        onUnmount={onUnmount}
+      >
+      </GoogleMap>
     </View>
-  )
+  ) : <Text>404 Dawg</Text>
 }
+
+const styles = StyleSheet.create({
+  mapStyle: {
+    height: 400,
+    width: 400,
+  }
+})
 
 export default HomeScreen;
